3dc7b4df6229eeb5a3446a6a433886f727eca3ef
MeonJiDust <sikong0685@gmail.com>
2022-01-28 20:52:43 +0900

    3-8 중간 커밋

---
+++ JavaAlgorithm/src/chap03/PhysExamSearch.java
@@ -0,0 +1,51 @@
+package chap03;
+
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class PhysExamSearch {
+	
+	static class PhyscData{
+		
+		private String name;
+		private int height;
+		private double vision;
+		
+		public PhyscData(String name, int height, double vision) {
+			
+			this.name = name;
+			this.height = height;
+			this.vision = vision;
+		}
+		
+		public String toString() {
+			
+			return name + " " + height + " " + vision;
+		}
+		
+		public static final Comparator<PhyscData> HEIGHT_ORDER = new HeightOrderCompartor();
+		
+		private static class HeightOrderCompartor implements Comparator<PhyscData>{
+			
+			public int compare(PhyscData d1, PhyscData d2) {
+				
+				return (d1.height > d2.height) ? 1 : (d1.height < d2.height) ? -1 : 0;
+			}
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		PhyscData[] xDatas = {
+				new PhyscData("이시현", 153, 0.7),
+				new PhyscData("박승우", 169, 1.2),
+				new PhyscData("문서영", 155, 1.0)
+		};
+
+	}
+
+}
