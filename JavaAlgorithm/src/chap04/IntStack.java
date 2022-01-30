ebb797a3105834aa9de99f8b7637ae1f869ea744
MeonJiDust <sikong0685@gmail.com>
2022-01-30 22:43:41 +0900

    실습 4-1[A]

--- JavaAlgorithm/src/chap04/IntStack.java
+++ JavaAlgorithm/src/chap04/IntStack.java
@@ -1,10 +1,27 @@
 package chap04;
 
 public class IntStack {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private int max;
+	private int ptr;
+	private int[] stk;
+	
+	public class EmptyIntStackException extends RuntimeException{
+		public EmptyIntStackException() {}
+	}
+	
+	public class OverflowIntStackException extends RuntimeException{
+		public OverflowIntStackException() {}
 	}
 
+	public IntStack(int capacity) {
+		
+		ptr = 0;
+		max = capacity;
+		try {
+			stk = new int[max];
+		}catch (OutOfMemoryError e) {
+			max = 0;
+		}
+	}
 }
