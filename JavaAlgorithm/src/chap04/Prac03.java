c73c0f3fadee0115ea1b9b7b0b221a19d2ef3fc6
MeonJiDust <sikong0685@gmail.com>
2022-02-08 20:50:31 +0900

    스택 공부1

---
+++ JavaAlgorithm/src/chap04/Prac03.java
@@ -0,0 +1,17 @@
+package chap04;
+
+public class Prac03 {
+	
+	private int max;
+	private int ptrA;
+	private int ptrB;
+	private int[] stk;
+	
+	public class EmptyStackException extends RuntimeException{
+		public EmptyStackException() {
+			
+		}
+	}
+	
+
+}
---
+++ JavaAlgorithm/src/chap04/Prac04.java
@@ -0,0 +1,25 @@
+package chap04;
+
+public class Prac04 {
+
+	private int max;
+	private int num;
+	private int[] que;
+	
+	public static class EmptyGqueException extends RuntimeException{
+		public EmptyGqueException() {
+			
+		}
+	}
+	
+	public static class OverflowGqueException extends RuntimeException{
+		public OverflowGqueException() {
+			
+		}
+	}
+	
+	public Prac04(int capacity) {
+		
+		
+	}
+}
